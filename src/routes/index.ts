08787133bae235dd3c793fc7a603ef8d176311fd
eliasall3x <elliasallex@gmail.com>
2021-04-15 19:37:51 -0300

    Initial Emitter

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import { agenciesRoutes } from "./agencies.routes";
-
-const routes = Router();
-
-routes.use("/agencies", agenciesRoutes);
-
-export { routes };
